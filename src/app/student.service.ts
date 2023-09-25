1f0a014105479290ce5c5fef2fb0563ab93f1210
unknown <divyanianerao97@gmail.com>
2023-09-25 16:21:55 +0530

    initial commit of front end project

--- src/app/student.service.ts
+++ src/app/student.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
+import { environment } from '../environments/environment'
 @Injectable({
   providedIn: 'root'
 })
@@ -10,10 +11,10 @@ export class StudentService {
   }
   title = 'detailsPortal';
   getStudents(){
-    return this.http.get('http://localhost:5000/students')
+    return this.http.get(environment.apiUrl+'students')
   }
 
   getStudentDetails(studentId:number = 0,newDate:any = 0){
-    return this.http.get('http://localhost:5000/students/details?id='+studentId+'&dates='+newDate)
+    return this.http.get(environment.apiUrl+'/students/details?id='+studentId+'&dates='+newDate)
   }
 }
